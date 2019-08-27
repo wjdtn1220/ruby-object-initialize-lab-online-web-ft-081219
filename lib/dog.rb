d08a7f067d332c8ba58a64bbf20ac47648985bf3
Jeong Park <wjdtn1220@gmail.com>
2019-08-27 00:43:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,6 @@
 class Dog
-  def initialize (dogs_name, dogs_breed = "Mutt")
-    @name = dogs_name
-    @breed = dogs_breed
+  def initialize(name, breed = "Mutt")
+    @name = name
+    @breed = breed
   end
-
-  def name=(dogs_name, dogs_breed = "Mutt")
-    @name = dogs_name
-    @breed = dogs_breed
-  end
-
-  def name
-    @name
-  end
-  def breed
-    @breed
-  end 
 end
-  
--- lib/person.rb
+++ lib/person.rb
@@ -1,14 +1,6 @@
 class Person
-  def initialize (persons_name)
-    @name = persons_name
+  def initialize(name)
+    @name = name
   end
-
-  def name=(persons_name)
-    @name = persons_name
-  end
-
-  def name
-    @name
-  end
-  
 end
+    
